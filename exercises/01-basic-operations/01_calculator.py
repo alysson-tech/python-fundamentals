8d2a22c06631273cdf2e72e4afbea9dd513399be
Alysson Bruno O. Santos <alyssonbrunooliveirasantos@gmail.com>
2025-04-17 23:22:18 -0300

    add 01_calculator.py file

---
+++ exercises/01-basic-operations/01_calculator.py
@@ -0,0 +1,9 @@
+# Calculator: addition, subtraction, multiplication, and division
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print(f"The sum is {num1 + num2}")
+print(f"The subtraction is {num1 - num2}")
+print(f"The multiplication is {num1 * num2}")
+print(f"The division is {num1 / num2:.2f}")
+
