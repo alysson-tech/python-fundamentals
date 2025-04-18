3c6cc68b1ddd9fede2ce860bba63e25ce823c75e
Alysson Bruno O. Santos <alyssonbrunooliveirasantos@gmail.com>
2025-04-17 23:23:16 -0300

    add 02_arithmetic_mean.py file

---
+++ exercises/01-basic-operations/02_arithmetic_mean.py
@@ -0,0 +1,7 @@
+# Arithmetic mean of three numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+mean = (num1 + num2 + num3) / 3
+print(f"The arithmetic mean is {mean:.2f}")
